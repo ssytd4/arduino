f9d429bda113b2a90aae8bc399b78e3378f0985e
Dai <ssytd4@nottingham.edu.cn>
2024-04-29 16:11:51 +0800

    add report

--- temp_prediction/temp_prediction.m
+++ temp_prediction/temp_prediction.m
@@ -4,27 +4,32 @@ function temp_prediction(board, sensorPin, greenLEDPin, yellowLEDPin, redLEDPin)
     TC = 10;    % mV/°C
     
     % Initialize temperature variables
-    temperatureHistory = zeros(1, 240); % Store last 60 seconds (240 samples)
+    currentTemp = 0;
+    temperatureHistory = zeros(1, 120); % Store last 2 minutes (120 samples)
 
     % Main loop
     cnt = 0;
     while true
         % Read temperature
         voltage = readVoltage(board, sensorPin);
+        prevTemp = currentTemp;
         currentTemp = (voltage*1000 - V0_C) / TC;
         
         % Update temperature history
         temperatureHistory = [temperatureHistory(2:end), currentTemp];
-        if cnt == 240
-            cnt = 240;
+        if cnt == 120
+            cnt = 120;
         else
             cnt = cnt + 1;
         end
         
-        if cnt == 240
+        if cnt == 120
+            % Calculate temperature change rate in °C/s
+            tempChangeRateSec = currentTemp - prevTemp;
+            
             % Calculate temperature change rate in °C/min
-            tempChangeRateMin = mean(diff(temperatureHistory)) * 60;
-        
+            tempChangeRateMin = mean(temperatureHistory(61:120)) - mean(temperatureHistory(1:60));
+            
             % Calculate predicted temperature in 5 minutes
             predictedTemp = currentTemp + (tempChangeRateMin * 5);
         
@@ -50,11 +55,13 @@ function temp_prediction(board, sensorPin, greenLEDPin, yellowLEDPin, redLEDPin)
         
             % Print current temperature and predicted temperature
             fprintf('Current Temperature: %.2f°C\n', currentTemp);
+            fprintf('Temperature Change Rate: %.2f°C/s\n', tempChangeRateSec);
+            fprintf('Temperature Change Rate: %.2f°C/min\n', tempChangeRateMin);
             fprintf('Predicted Temperature in 5 Minutes: %.2f°C\n', predictedTemp);
-            fprintf('Temperature ChangeRate: %.2f°C/min\n', tempChangeRateMin);
+            fprintf('\n');
         end
         
-        % Wait for 0.25 seconds
-        pause(0.25);
+        % Wait for 1 seconds
+        pause(1);
     end
 end
